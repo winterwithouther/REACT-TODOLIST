f63038c36718f8d5895325aad3d04230f1270451
EmanGurung <136867625+winterwithouther@users.noreply.github.com>
2024-07-29 21:32:36 -0400

    added functionality to persist todo list data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function App() {
 
   const [todos, setTodos] = useState([])
   const [todoValue, setTodoValue] = useState("")
 
+  function persistData(newList) {
+    localStorage.setItem("todos", JSON.stringify({todos : newList}))
+  }
+
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
 
@@ -16,8 +21,23 @@ function App() {
     const newTodoList = todos.filter((todo, todoIndex) => {
       return todoIndex !== index
     })
+    persistData(newTodoList)
     setTodos(newTodoList)
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem("todos")
+    if (!localTodos) {
+      return
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
 
   function handleEditTodo(index) {
     const valueToBeEdited = todos[index]
